1bbaef48ae482714d83c3087b5b403884b6e7bd8
SakharovaKsu <nagis38@gmail.com>
2023-03-20 23:57:41 +0300

    Стилизация шапки и первого экрана

---
+++ source/js/modules/header-nav.js
@@ -0,0 +1,11 @@
+export const navFooter = () => {
+  let headerNav = document.querySelector('.header__nav');
+  let headerButton = document.querySelector('.header__button');
+
+  headerNav.classList.remove('header__nav--nojs');
+
+  headerButton.addEventListener('click', function () {
+    headerNav.classList.toggle('header__nav--closed');
+    headerNav.classList.toggle('header__nav--opened');
+  });
+};
