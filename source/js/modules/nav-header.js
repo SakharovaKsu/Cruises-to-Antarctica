c2c8a3800c11801e129e604ae06a9af5aaf50122
SakharovaKsu <nagis38@gmail.com>
2023-03-26 23:50:13 +0300

    Сжатие webp, переделка бургера в js

--- source/js/modules/nav-header.js
+++ source/js/modules/nav-header.js
@@ -1,11 +1,28 @@
 export const navHeader = () => {
-  let headerNav = document.querySelector('.header__nav');
-  let headerButton = document.querySelector('.header__button');
+  const headerNav = document.querySelector('.header__nav');
+  const headerButton = document.querySelector('.header__button');
+  const headerLink = document.querySelectorAll('.header__link');
+  const headerOverlay = document.querySelector('.header__overlay');
 
   headerNav.classList.remove('header__nav--nojs');
 
-  headerButton.addEventListener('click', function () {
+  const onToggleMenu = () => {
     headerNav.classList.toggle('header__nav--closed');
     headerNav.classList.toggle('header__nav--opened');
+    document.body.classList.toggle('scroll-lock');
+  };
+
+  headerButton.addEventListener('click', function () {
+    onToggleMenu();
+  });
+
+  headerOverlay.addEventListener('click', function () {
+    headerNav.classList.remove('header__nav--opened');
+  });
+
+  headerLink.forEach((link) => {
+    link.addEventListener('click', function () {
+      onToggleMenu();
+    });
   });
 };
