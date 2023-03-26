2bb7b285f86aad2793814ff4a6cfe945dffe286c
SakharovaKsu <nagis38@gmail.com>
2023-03-26 21:50:59 +0300

    Доработка меню в мобилке,убрала лишние комментария
    Доработала состояния соц. сетей

--- source/js/main.js
+++ source/js/main.js
@@ -1,7 +1,7 @@
 import {iosVhFix} from './utils/ios-vh-fix';
 import {Form} from './modules/form-validate/form';
 import {initPhoneMask} from './vendor/phone-mask';
-import {navHeader} from './modules/header-nav';
+import {navHeader} from './modules/nav-header';
 import {initMap} from './modules/map';
 import {scrollPage} from './modules/scroll-page';
 
@@ -39,8 +39,6 @@ window.addEventListener('DOMContentLoaded', () => {
   // Modules
   // ---------------------------------
 
-  // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
-  // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
     initPhoneMask();
     const form = new Form();
@@ -48,28 +46,3 @@ window.addEventListener('DOMContentLoaded', () => {
     form.init();
   });
 });
-
-// ---------------------------------
-
-// ❗❗❗ обязательно установите плагины eslint, stylelint, editorconfig в редактор кода.
-
-// привязывайте js не на классы, а на дата атрибуты (data-validate)
-
-// вместо модификаторов .block--active используем утилитарные классы
-// .is-active || .is-open || .is-invalid и прочие (обязателен нейминг в два слова)
-// .select.select--opened ❌ ---> [data-select].is-open ✅
-
-// выносим все в дата атрибуты
-// url до иконок пинов карты, настройки автопрокрутки слайдера, url к json и т.д.
-
-// для адаптивного JS используется matchMedia и addListener
-// const breakpoint = window.matchMedia(`(min-width:1024px)`);
-// const breakpointChecker = () => {
-//   if (breakpoint.matches) {
-//   } else {
-//   }
-// };
-// breakpoint.addListener(breakpointChecker);
-// breakpointChecker();
-
-// используйте .closest(el)
